d295c6f20533a91bfc8557d99dc4fca2dffbddb8
Faisalse <faisalaslam156@gmail.com>
2020-07-31 03:35:38 -0700

    hi

---
+++ Assets/Scripts/languageChosen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class languageChosen : MonoBehaviour
+{
+    public Text palindromeLanguage;
+    public Text pDALanguage_textField;
+    string palLan = "XF3";
+    string pDALan = "(XF3)";
+    public void palindromeLanguag()
+    {
+        palindromeLanguage.text = palLan.ToString();
+    }
+
+    public void PDALanguag()
+    {
+        pDALanguage_textField.text = pDALan.ToString();
+    }
+}
